b17feb6b545226b9ca0de347779725d88d004fff
Jochen Maes <jochen@sejo-it.be>
2023-11-01 10:41:38 +0100

    improves system
    only show what we need.

--- src/gatherers/system.rs
+++ src/gatherers/system.rs
@@ -12,14 +12,15 @@ impl SystemData {}
 impl Fact for SystemData {
     fn gather(&self) -> String {
         if System::IS_SUPPORTED {
-            let mut sys = System::new_all();
+            let mut sys = System::new();
             // we only need certain values.
             sys.refresh_cpu();
             sys.refresh_disks();
+            sys.refresh_disks_list();
             sys.refresh_memory();
             sys.refresh_system();
             sys.refresh_components();
-            sys.refresh_disks_list();
+            sys.refresh_components_list();
             sys.refresh_users_list();
             serde_json::to_string(&sys).unwrap()
         } else {
