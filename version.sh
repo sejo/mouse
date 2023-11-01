e7e0720b54fa0606a64988dd7d3a264bff6b0a35
Jochen Maes <jochen@sejo-it.be>
2023-11-01 08:41:33 +0100

    adds version.sh to do some version manipulation during build

---
+++ version.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Get last created tag
+LTCOMMIT=$(git rev-list --abbrev-commit --tags --max-count=1)
+LTAG=$(git describe --abbrev=0 --tags ${LTCOMMIT} 2>/dev/null || true)
+ADC=${DRONE_COMMIT:0:7}
+
+if [[ "${LTCOMMIT}" == "${ADC}" ]]; then
+	VERSION="${DRONE_TAG##v}"
+else
+	VERSION="${LTAG}-next-${ADV}"
+fi
+
+sed -i "s/0\\.0\\.0/${VERSION}/" Cargo.toml
